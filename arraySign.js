d89f2ea88bb14c49e5a859afb78ba6b559f1f2fa
Deb Johnson <friendswithdrj@gmail.com>
2021-05-04 14:56:20 -0500

    Save progress

---
+++ arraySign.js
@@ -0,0 +1,52 @@
+/*
+There is a function signFunc(x) that returns:
+
+1 if x is positive.
+-1 if x is negative.
+0 if x is equal to 0.
+You are given an integer array nums. Let product be the product of all values in the array nums.
+
+Return signFunc(product).
+
+Example 1:
+
+Input: nums = [-1,-2,-3,-4,3,2,1]
+Output: 1
+Explanation: The product of all values in the array is 144, and signFunc(144) = 1
+Example 2:
+
+Input: nums = [1,5,0,2,-3]
+Output: 0
+Explanation: The product of all values in the array is 0, and signFunc(0) = 0
+Example 3:
+
+Input: nums = [-1,1,-1,1,-1]
+Output: -1
+Explanation: The product of all values in the array is -1, and signFunc(-1) = -1
+
+Constraints:
+
+1 <= nums.length <= 1000
+-100 <= nums[i] <= 100
+*/
+
+const arraySign = (nums) => {
+  // find product
+  let product = nums.reduce((acc, val) => acc * val);
+  // check if product is positive
+  //   yes, return 1
+  // product negative return -1
+  // product 0 return 0
+  return product;
+};
+
+const test = () => {
+  const example1 = arraySign([-1,-2,-3,-4,3,2,1]);
+  console.log(example1);
+  const example2 = arraySign([1,5,0,2,-3]);
+  console.log(example2);
+  const example3 = arraySign([-1,1,-1,1,-1]);
+  console.log(example3);
+};
+
+test();
